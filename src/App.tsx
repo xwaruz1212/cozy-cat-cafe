e86ffef56a01b5a3c58934abe99be31c774fa390
Kacper Żurawski <ikswaruz@gmail.com>
2023-09-15 21:26:22 +0200

    Add website theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import { ThemeProvider } from "@emotion/react";
+import { createTheme } from "@mui/material/styles";
+import React from "react";
 
-function App() {
+let theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ceb995",
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: "#f6e6bf",
+      contrastText: "#000",
+    },
+  },
+  typography: {
+    fontFamily: ["Inter", "sans-serif"].join(","),
+    htmlFontSize: 12,
+  },
+});
+
+theme.typography.h1 = {
+  fontSize: 50,
+  fontWeight: 500,
+};
+
+theme.typography.h2 = {
+  fontSize: 30,
+  fontWeight: 300,
+};
+
+const App: React.FC = () => {
   return (
-    <div>
-        <div>APP SETUP</div>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <h1>APP SETUP</h1>
+      </div>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
