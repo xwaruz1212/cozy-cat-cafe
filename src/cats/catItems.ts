038f49e4b5c00eb87e4ecd7d837a8ea9799c31e1
Wiktoria Wlodarczyk <wlodarczykw033@gmail.com>
2023-11-07 22:27:10 +0100

    items

---
+++ src/cats/catItems.ts
@@ -0,0 +1,40 @@
+
+export const catItems = 
+[
+  {
+    img: "matcha.jfif", 
+    name: "Leo", 
+    note: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Undperspiciatis ipsa temporibus voluptatem nihil, eligendi voluptatofficiis nesciunt", 
+    
+  },
+  {
+    img: "coffe.jfif",
+    name: "Teo",
+    note: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Undperspiciatis ipsa temporibus voluptatem nihil, eligendi voluptatofficiis nesciunt",
+    
+  },
+  {
+    img: "curry.jfif",
+    name: "Mia",
+    note: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Undperspiciatis ipsa temporibus voluptatem nihil, eligendi voluptatofficiis nesciunt",
+    
+  },
+  {
+    img: "onion.png",
+    name: "Tom",
+    note: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Undperspiciatis ipsa temporibus voluptatem nihil, eligendi voluptatofficiis nesciunt",
+    
+  },
+  {
+    img: "tacos.jfif",
+    name: "Jerry",
+    note: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Undperspiciatis ipsa temporibus voluptatem nihil, eligendi voluptatofficiis nesciunt",
+    
+  },
+  {
+    img: "tofu.jfif",
+    name: "Lila",
+    note: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Undperspiciatis ipsa temporibus voluptatem nihil, eligendi voluptatofficiis nesciunt",
+    
+  },
+];
--- src/cats/index.tsx
+++ src/cats/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box, Container, Typography, useTheme } from "@mui/material";
-
+import { catItems } from "./catItems";
 
 const Cats: React.FC = () => {
   return(
@@ -8,46 +8,113 @@ const Cats: React.FC = () => {
     <Container
      maxWidth="xl"
      sx={{
-       bgColor: "#ceb995",
+       backgroundColor: "#f6e6bf",
        display: "flex",
-       flexDirection: "column",
-       height: "100%",
+       flexDirection: "row",
+       height: "90vh",
        width: "100%",
        position: "relative",
+       alignItems: "center",
+       alignContent: "center",
      }}>
+      
+          
       <Box
       sx={{
-        width: "100%",
-        left: "32px",
-        paddingTop: "100px",
+        width: "50%",
+        paddingTop: "32px",
         paddingBottom: "100px",
+        paddingRight: "32px",
         display: "flex",
-        flexDirection: "row",
-        height: "100%",
-        
-        
-        
+        flexDirection: "column",
+        position: "relative"
       }}>
-        <Typography variant="h1">We love cats</Typography>
-          <br />
+         <Typography variant="h1">We love cats</Typography>
+          
           <Typography variant="h4">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
             perspiciatis ipsa temporibus voluptatem nihil, eligendi voluptatum
             officiis nesciunt officia fugit magni consequuntur vero adipisci
             expedita, ratione pariatur accusamus et vitae!
           </Typography>
+         
           
-            <img
-              src={require('../assets/menu/chef_cat.jpg')}
-              style={{maxHeight: "400px", width: "600px"}}
-              
-            />
-             
+          
+          
+          
           </Box>
+          <img 
+              src={require('../assets/menu/chef_cat.jpg')}
+              style={{maxHeight: "400px", width: "50%"}}
+
+            />
     </Container>
+    <Container
+     maxWidth="xl"
+     disableGutters={true}
+     sx={{
+      backgroundColor: "white", 
+      width: "100%", 
+      display: "flex", 
+      flexDirection: "column",
+      alignItems: "center",
+      alignContent: "center",
+      gap: "32px",
+      paddingBottom: "32px",
+      paddingTop: "32px"
+      
+     }}>
+      <Typography variant="h1">Our cats</Typography>
+     <Box
+        sx={{
+          display: "grid", 
+          gridTemplateColumns: "1fr 1fr",
+          flexDirection: "column",
+          alignItems: "center",
+          alignContent: "center",
+          gap: "32px",
+        }}>
+        {catItems.map((catItem) => ( 
+            <Box
+             sx={{
+               display: "flex",
+               flexDirection: "row",
+               alignItems: "center",
+               alignContent: "center",
+               marginTop: "20px",
+               marginBottom: "20px",
+               gap: "30px",
+              }}
+            >
+              <img
+                srcSet={require("../assets/menu/" + catItem.img)}
+                src={require("../assets/menu/" + catItem.img)}
+                alt={catItem.name}
+                loading="lazy"
+                style={{
+                height: "40vh",
+                width: "15vw",
+               
+                
+                }}
+              />
+              <Box
+               sx={{
+                  width: "15vw",
+                  cursor: "default",
+                }}
+              >
+                <Typography variant="h5" style={{color: "green", }}>{catItem.name}</Typography>
+                <Typography variant="h2" style={{fontSize: "16px"}}>
+                  {catItem.note} 
+                </Typography>
+              </Box>
+            </Box>
+          ))}
+      </Box>
+    </Container> 
   
   </>
   );
 };
-
 export default Cats;
